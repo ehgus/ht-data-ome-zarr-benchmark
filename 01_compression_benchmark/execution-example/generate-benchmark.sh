708565018014bbe84c06e4a295388908789e5065
Dohyeon Lee <dleh428@kaist.ac.kr>
2025-02-24 20:45:27 +0900

    enable saving sliced images

--- 01_compression_benchmark/execution-example/generate-benchmark.sh
+++ 01_compression_benchmark/execution-example/generate-benchmark.sh
@@ -1,4 +1,4 @@
 
 uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/embryo.ome.zarr ./data/test-embryo-8G 1024 "(1,1,32,256,256)" "(0,0,0,5,5)" --channel 0
-uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-on-8G 1024 "(1,1,32,256,256)" "(0,0,0,20,40)" --channel 0
-uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-off-8G 1024 "(1,1,32,256,256)" "(0,0,0,20,40)" --channel 1
+uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-on-8G 1024 "(1,1,32,256,256)" "(0,0,0,540,200)" --channel 0
+uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-off-8G 1024 "(1,1,32,256,256)" "(0,0,0,540,200)" --channel 1
