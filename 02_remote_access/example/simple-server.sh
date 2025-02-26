8df19e893b4ec499aa87ecfa5f06a089dd604696
Dohyeon Lee <dleh428@kaist.ac.kr>
2025-02-26 16:39:22 +0900

    add more comment on remote server example

--- 02_remote_access/example/simple-server.sh
+++ 02_remote_access/example/simple-server.sh
@@ -1,3 +1,5 @@
 
 uv run python 02_remote_access/simple-server.py ./data/
-# in napari, run "viewer.open('http://localhost:8000/embryo.ome.zarr/',plugin='napari-ome-zarr')"
+# in napari, run "viewer.open('http://localhost:8000/birefringent-data.ome.zarr/',plugin='napari-ome-zarr')"
+# To connect through SSH/SCP, change the URL to 'ssh://<USERNAME>@localhost:<PORT>/birefringent-data.ome.zarr/' where USERNAME and PORT is your custom value.
+# It is possible to use object storage
