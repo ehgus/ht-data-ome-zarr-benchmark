4099d3edbdd2131e3d96edcfdbbc9f2c37fe6cef
Dohyeon Lee <dleh428@kaist.ac.kr>
2025-03-25 19:31:19 +0900

    correct bulk-benchmark example

--- 01_compression_benchmark/execution-example/bulk-benchmark.sh
+++ 01_compression_benchmark/execution-example/bulk-benchmark.sh
@@ -1,6 +1,6 @@
 
-$benchmark_code="01_compression_benchmark/benchmark_sampled_compression_preset_bulk.py"
-$benchmark_recipe="01_compression_benchmark/benchmark_recipe.toml"
+export benchmark_code="01_compression_benchmark/benchmark_sampled_compression_preset_bulk.py"
+export benchmark_recipe="01_compression_benchmark/benchmark_recipe.toml"
 
 uv run python $benchmark_code $benchmark_recipe data/test-embryo-8G output/bench-embryo-8G
 uv run python $benchmark_code $benchmark_recipe data/test-tissue-on-8G output/bench-tissue-on-8G
