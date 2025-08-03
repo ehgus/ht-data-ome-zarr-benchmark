417cd0500eea45787b368502cf1de55a95090a10
Dohyeon Lee <dleh428@kaist.ac.kr>
2025-08-03 14:54:31 +0900

    add organoid and HeLa in benchmark

--- 01_compression_benchmark/execution-example/bulk-benchmark.sh
+++ 01_compression_benchmark/execution-example/bulk-benchmark.sh
@@ -5,3 +5,5 @@ export benchmark_recipe="01_compression_benchmark/benchmark_recipe.toml"
 uv run python $benchmark_code $benchmark_recipe data/test-embryo-8G output/bench-embryo-8G
 uv run python $benchmark_code $benchmark_recipe data/test-tissue-on-8G output/bench-tissue-on-8G
 uv run python $benchmark_code $benchmark_recipe data/test-tissue-off-8G output/bench-tissue-off-8G
+uv run python $benchmark_code $benchmark_recipe data/test-organoid-8G output/bench-organoid-8G
+uv run python $benchmark_code $benchmark_recipe data/test-HeLa-8G output/bench-HeLa-8G
--- 01_compression_benchmark/execution-example/generate-benchmark.sh
+++ 01_compression_benchmark/execution-example/generate-benchmark.sh
@@ -2,3 +2,5 @@
 uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/embryo.ome.zarr ./data/test-embryo-8G 1024 "(1,1,32,256,256)" "(0,0,0,5,5)" --channel 0
 uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-on-8G 1024 "(1,1,32,256,256)" "(0,0,0,540,200)" --channel 0
 uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/birefringent-data.ome.zarr ./data/test-tissue-off-8G 1024 "(1,1,32,256,256)" "(0,0,0,540,200)" --channel 1
+uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/HeLa.ome.zarr ./data/test-HeLa-8G 1024 "(1,1,32,256,256)" "(0,0,0,5,5)" --channel 0
+uv run python ./01_compression_benchmark/generate_benchmark_sample.py ./data/organoid.ome.zarr ./data/test-organoid-8G 1024 "(1,1,32,256,256)" "(0,0,0,5,5)" --channel 0
